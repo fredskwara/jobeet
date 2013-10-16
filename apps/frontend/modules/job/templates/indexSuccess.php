13e5362a42af0265089c384ae01e951e9c9aed1b
fredskwara <fredskwara@gmail.com>
2013-10-16 16:23:04 -0300

    tutorial day 7

---
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -0,0 +1,34 @@
+<h1>Jobeetjobs List</h1>
+
+<div id="jobs">
+  <?php foreach ($categories as $category): ?>
+    <div class="category_<?php echo Jobeet::slugify($category->getName()) ?>">
+      <div class="category">
+        <div class="feed">
+          <a href="">Feed</a>
+        </div>
+        <h1><?php echo $category ?></h1>
+      </div>
+ 
+      <table class="jobs">
+        <?php foreach ($category->getActiveJobs(sfConfig::get('app_max_jobs_on_homepage')) as $i => $job): ?>
+          <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
+            <td class="location">
+              <?php echo $job->getLocation() ?>
+            </td>
+            <td class="position">
+              <?php echo link_to($job->getPosition(), 'job_show_user', $job) ?>
+            </td>
+            <td class="company">
+              <?php echo $job->getCompany() ?>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </table>
+    </div>
+  <?php endforeach; ?>
+</div>
+	</table>
+</div>
+
+  <a href="<?php echo url_for('job/new') ?>">New</a>
